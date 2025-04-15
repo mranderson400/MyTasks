7b2b284758f7d93e5b63e4f78463a27acc2bd68b
mranderson400 <mr_andersonr97@yahoo.com>
2025-04-14 23:42:25 -0400

    deployment

--- server.py
+++ server.py
@@ -20,3 +20,7 @@ app.include_router(chatbot_controller.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5001, reload=True)
+
+# Vercel deployment
+from mangum import Mangum
+handler = Mangum(app)
